db30a453bc02a5a669bb5ac79a5397b48b6f8b6e
akramoo <samirounis2@gmail.com>
2025-11-07 21:20:35 +0100

    fix: update layout and styling for TargetItem component for better responsiveness and visual consistency

--- app/(landing)/_components/TargetItem.tsx
+++ app/(landing)/_components/TargetItem.tsx
@@ -8,12 +8,12 @@ type TargetItemProps = {
 export default function TargetItem({ title }: TargetItemProps) {
   return (
     <li
-      className="flex group text-xl items-center hover:text-white transition-all"
+      className="flex group text-base md:text-lg lg:text-xl items-center hover:text-white transition-all"
     >
-      <ChevronRight className="text-[#6b63ff] w-6 h-6 group-hover:translate-x-4 transition-all" />
+      <ChevronRight className="text-[#6b63ff] w-5 h-5 md:w-6 md:h-6 group-hover:translate-x-2 lg:group-hover:translate-x-4 transition-all" />
 
-      <FuzzyText enableHover fontSize='24px' baseIntensity={.01} hoverIntensity={.2}>
-        {title}
+      <FuzzyText enableHover fontSize='16px' baseIntensity={.01} hoverIntensity={.2}>
+        <span className="md:text-lg lg:text-xl">{title}</span>
       </FuzzyText>
 
     </li>
